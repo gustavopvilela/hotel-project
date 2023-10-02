c27a17a2a0441183f740b8cb7a9fe7de6d05d007
unknown <Alunos@LAB04-PC18.academico.formiga.ifmg.local>
2023-10-02 11:55:11 -0300

    branch reserva

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -36,7 +36,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/gestao_dados.o \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/reserva.o
 
 
 # C Compiler Flags
@@ -72,6 +73,11 @@ ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+
+${OBJECTDIR}/reserva.o: reserva.c
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/reserva.o reserva.c
 
 # Subprojects
 .build-subprojects:
---
+++ reserva.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   reserva.h
+ * Author: Alunos
+ *
+ * Created on 2 de outubro de 2023, 11:23
+ */
+
+#ifndef RESERVA_H
+#define RESERVA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RESERVA_H */
+
